fba7a11212dbb6964cfe5ea17b8bba13cd1dcf2e
Joseph <josephsasson1@gmail.com>
2021-06-14 14:48:12 -0400

    commit tree 4dcd79e9d55c23a2b8be68039ae39cbe7ae0d2e2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(event = 'roller-skate'){
+  return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(event = 'go to the office'){
+  return `This Monday, I will ${event}.`
+}
+
+function wrapAdjective(flair = '*'){
+  return function func(word = 'special'){
+    return `You are ${flair}${word}${flair}!`
+  }
+}
